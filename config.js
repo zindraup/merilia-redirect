86fef740d2763385bdbfeae51563bd0ea4de5d69
zindraup <saintdrop.music@gmail.com>
2026-05-02 20:12:05 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -8,10 +8,10 @@ const CONFIG = {
   title: "Envie",
   cover: "cover.jpg",
   links: {
-    spotify: "https://open.spotify.com/intl-fr/track/0mNGJ5sdhPVdp7AQiwWz2y?si=5e42aaeda5124f12",
+    spotify: "https://spotify.openinapp.co/31xu3",
     youtube: "https://yt.openinapp.co/c5d74",
     deezer: "https://openinapp.link/deezermeriliaenvie",
-    amazon: "https://music.amazon.fr/albums/B0GY62S763?marketplaceId=A13V1IB3VIYZZH&musicTerritory=FR&ref=dm_sh_yGdKrg81QoKag3RXXluawFuVn&trackAsin=B0GY5V26SD",
+    amazon: "https://amzn.openinapp.link/ugxi2",
     apple: "https://applemusic.openinapp.co/od4mg",
     tidal: "https://openinapp.link/tidalenvie",
     qobuz: "https://openinapp.link/qobuz"

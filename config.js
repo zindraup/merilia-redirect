979594a081742241992b5394d6d8a59dceb0b47e
zindraup <saintdrop.music@gmail.com>
2026-08-01 02:11:48 +0200

    detail

--- config.js
+++ config.js
@@ -5,8 +5,54 @@
 
 const CONFIG = {
   artist: "Merilia",
-  title: "Envie",
-  cover: "cover.jpg",
+
+  // Nouveau single ("Détail")
+  currentRelease: {
+    title: "Détail",
+    badge: "NOUVEAU SINGLE - PRE-SAVE",
+    cover: "cover_detail.jpg",
+    releaseDate: "2026-08-15T00:00:00",
+    showSpotifyPreSave: false, // Afficher ou non l'option de pre-save Spotify après que l'utilisateur a rejoint la liste (par défaut: false)
+    spotifyPreSaveUrl: "https://spotify.openinapp.co/31xu3",
+    preSaveUrl: "https://spotify.openinapp.co/31xu3",
+    spotifyIosUrl: "https://open.spotify.com/intl-fr/track/0mNGJ5sdhPVdp7AQiwWz2y?si=33753fe9380c4f25",
+    links: {
+      spotify: "https://spotify.openinapp.co/31xu3",
+      spotify_ios: "https://open.spotify.com/intl-fr/track/0mNGJ5sdhPVdp7AQiwWz2y?si=33753fe9380c4f25",
+      youtube: "https://yt.openinapp.co/c5d74",
+      deezer: "https://openinapp.link/deezermeriliaenvie",
+      amazon: "https://amzn.openinapp.link/ugxi2",
+      apple: "https://applemusic.openinapp.co/od4mg",
+      tidal: "https://openinapp.link/tidalenvie",
+      qobuz: "https://openinapp.link/qobuz"
+    }
+  },
+
+  // Titre précédent disponible à l'écoute ("Envie")
+  previousRelease: {
+    title: "Envie",
+    badge: "SINGLE PRÉCÉDENT",
+    cover: "cover_envie.jpg",
+    links: {
+      spotify: "https://spotify.openinapp.co/31xu3",
+      spotify_ios: "https://open.spotify.com/intl-fr/track/0mNGJ5sdhPVdp7AQiwWz2y?si=33753fe9380c4f25",
+      youtube: "https://yt.openinapp.co/c5d74",
+      deezer: "https://openinapp.link/deezermeriliaenvie",
+      amazon: "https://amzn.openinapp.link/ugxi2",
+      apple: "https://applemusic.openinapp.co/od4mg",
+      tidal: "https://openinapp.link/tidalenvie",
+      qobuz: "https://openinapp.link/qobuz"
+    }
+  },
+
+  socials: {
+    instagram: "https://instagram.com/merilia_dnb",
+    tiktok: "https://tiktok.com/@merilia_dnb"
+  },
+
+  // Propriétés de rétrocompatibilité
+  title: "Détail",
+  cover: "cover_detail.jpg",
   links: {
     spotify: "https://spotify.openinapp.co/31xu3",
     spotify_ios: "https://open.spotify.com/intl-fr/track/0mNGJ5sdhPVdp7AQiwWz2y?si=33753fe9380c4f25",
